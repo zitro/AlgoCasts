cd21ff182902e47cc5e34bd090f5a31a3b65cbce
zitro <bryan@bryanortiz.com>
2018-07-14 01:06:29 -0400

    fizzBuzz, maxChar, palindrome

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,23 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+	obj = {};
+	arr = str.split('');
+	maxVal = 0;
+	maxChar = '';
+
+	for(let char of arr){
+		obj[char] ? obj[char] ++: obj[char] = 1;
+	}
+
+	for(let item in obj){
+		if (obj[item] > maxVal){
+			maxVal = obj[item]
+			maxChar = item
+		}
+	}
+	return maxChar;
+}
 
 module.exports = maxChar;
--- exercises/palindrome/index.js
+++ exercises/palindrome/index.js
@@ -7,6 +7,33 @@
 //   palindrome("abba") === true
 //   palindrome("abcdefg") === false
 
-function palindrome(str) {}
+function palindrome(str) {
+return str.split('').every((char, i)=> {
+	return char === str[str.length - i - 1];
+});
+
+}
 
 module.exports = palindrome;
+
+
+//try 1
+// function palindrome(str) {
+// 	reverse = str.split('').reverse().join('')
+// 	if(str !== reverse){
+// 		return false;
+// 	}else if(str.length % 2 === 0){
+// 		return true;
+// 	}else{
+// 		return true;
+// 	}
+//
+// }
+
+
+//refactored
+// function palindrome(str) {
+// 	reverse = str.split('').reverse().join('')
+// 	return str === reverse;
+//
+// }
