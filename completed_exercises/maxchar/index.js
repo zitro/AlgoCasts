3cd4fc7155ba3386a023d85604952290e61d195a
zitro <bryan@bryanortiz.com>
2018-08-01 18:51:32 -0400

    maxChar again

--- completed_exercises/maxchar/index.js
+++ completed_exercises/maxchar/index.js
@@ -6,26 +6,45 @@
 // maxChar("apple 1231111") === "1"
 
 function maxChar(str) {
-  const charMap = {};
-  let max = 0;
-  let maxChar = '';
+	let max = 0;
+	let maxChar = '';
+	let obj = {};
 
-  for (let char of str) {
-    if (charMap[char]) {
-      charMap[char]++;
-    } else {
-      charMap[char] = 1;
-    }
-  }
+	for (let char of str){
+		obj[char] = obj[char] + 1 || 1;
+	}
 
-  for (let char in charMap) {
-    if (charMap[char] > max) {
-      max = charMap[char];
-      maxChar = char;
-    }
-  }
+	for (let char in obj){
+		if(obj[char] > max){
+			max = obj[char];
+			maxChar = char;
+			
+		}
+	}
 
-  return maxChar;
 }
 
 module.exports = maxChar;
+
+
+
+// const charMap = {};
+// let max = 0;
+// let maxChar = '';
+//
+// for (let char of str) {
+// 	if (charMap[char]) {
+// 		charMap[char]++;
+// 	} else {
+// 		charMap[char] = 1;
+// 	}
+// }
+//
+// for (let char in charMap) {
+// 	if (charMap[char] > max) {
+// 		max = charMap[char];
+// 		maxChar = char;
+// 	}
+// }
+//
+// return maxChar;
